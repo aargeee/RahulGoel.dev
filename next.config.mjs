bc3ea880e61ed96f5181bce046cb3ce82590aabf
Rahul-NITD <rahul.goel@pixelotech.com>
2024-03-01 03:51:21 +0530

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rahulgoel-dev.vercel.app",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
