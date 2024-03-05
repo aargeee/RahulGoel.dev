e88b750a03b07c2a7a9c5aec5d291b4936f15979
Rahul-NITD <211220042@nitdelhi.ac.in>
2024-03-05 18:17:19 +0530

    content update

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -1,3 +1,4 @@
+import { ProjectCard } from "@/components/chat/project-card";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import { ReactNode } from "react";
@@ -62,9 +63,10 @@ export const userData = [
             message: (
               <p>
                 Sure! <br />
-                {`I'm`} <span className="font-semibold">Rahul Goel</span>. I love
-                development, I love finding out new ways to solve problems and
-                getting things done and just playing with everything I can grab.
+                {`I'm`} <span className="font-semibold">Rahul Goel</span>. I
+                love development, I love finding out new ways to solve problems
+                and getting things done and just playing with everything I can
+                grab.
                 <br />
                 Most Importantly, I know how to code and I know how to do it
                 correctly.
@@ -94,7 +96,11 @@ export const userData = [
                 <p>A full list of my skills are as follows</p>
                 <div className="skills flex flex-wrap gap-1">
                   {skills.map((skill, index) => (
-                    <Badge key={index} className="bg-white text-white" variant={"outline"}>
+                    <Badge
+                      key={index}
+                      className="bg-white text-white"
+                      variant={"outline"}
+                    >
                       <Image
                         src={skill.image}
                         alt={skill.name}
@@ -110,97 +116,116 @@ export const userData = [
           {
             avatar: "/LoggedInUser.jpg",
             name: "Jakob Hoeg",
-            message: "I am good, you?",
+            message: "Wow, good job! Are you trying to learn something new?",
           },
           {
             avatar: "/avatar.gif",
             name: RG,
-            message: "I am good too!",
+            message: (
+              <div>
+                <p className="line-through text-gray-400">making friends</p>
+                <p>I'm tring DevOps and Machine Learning as well. Two wildly different areas, I know, but current market trends tell me doing this is the safest bet.</p>
+              </div>
+            ),
           },
         ],
       },
       {
-        title: "Something Else",
+        title: "Projects",
         messages: [
           {
             avatar: "/LoggedInUser.jpg",
             name: "Jakob Hoeg",
-            message: "Hey!",
+            message:
+              "Your skills are impressive! Do you have any supporting evidence for your skills mentioned?",
           },
           {
             avatar: "/avatar.gif",
             name: RG,
-            message: "How are you?",
+            message: (
+              <div>
+                <p>
+                  You bet I do! <br />
+                  <span className="text-xs text-gray-400">
+                    This is only a fraction of all the stuff I did that I felt
+                    to be mature enough to have space here. Visit the portfolio
+                    page for a comprehensive list.
+                  </span>
+                </p>
+                <div className="flex flex-col">
+                  <ProjectCard />
+                  <ProjectCard />
+                  <ProjectCard />
+                </div>
+              </div>
+            ),
           },
           {
             avatar: "/LoggedInUser.jpg",
             name: "Jakob Hoeg",
-            message: "I am good, you?",
+            message: "Awesome! You seem to really know your stuff",
           },
           {
             avatar: "/avatar.gif",
             name: RG,
-            message: "I am good too!",
+            message: "Thanks!😊",
           },
         ],
       },
       {
-        title: "Something Else",
+        title: "Education",
         messages: [
           {
             avatar: "/LoggedInUser.jpg",
             name: "Jakob Hoeg",
-            message: "Hey!",
+            message:
+              "Do you have any formal education or courses you want to talk about?",
           },
           {
             avatar: "/avatar.gif",
             name: RG,
-            message: "How are you?",
-          },
-          {
-            avatar: "/LoggedInUser.jpg",
-            name: "Jakob Hoeg",
-            message: "I am good, you?",
-          },
-          {
-            avatar: "/avatar.gif",
-            name: RG,
-            message: "I am good too!",
+            message: <p>National Institute of Technology, Delhi</p>,
           },
         ],
       },
       {
-        title: "Something Else",
+        title: "Get in touch",
         messages: [
           {
             avatar: "/LoggedInUser.jpg",
             name: "Jakob Hoeg",
-            message: "Hey!",
+            message: "Cool! I'd like to get in touch with you",
           },
           {
             avatar: "/avatar.gif",
             name: RG,
-            message: "How are you?",
+            message: <div>
+              <p>Here are a few ways you can contact me!</p>
+              <p>rgoel1729@gmail.com</p>
+            </div>,
           },
           {
             avatar: "/LoggedInUser.jpg",
             name: "Jakob Hoeg",
-            message: "I am good, you?",
+            message: "Awesome! my boss is gonna love you in our team. Do you have a resume that I can share?",
           },
           {
             avatar: "/avatar.gif",
             name: RG,
-            message: "I am good too!",
+            message: <div>
+              <p>Thanks for the kind words😊</p>
+              <p>Here is my resume <a href="https://drive.google.com/file/d/1jEWOiY0IWr3VFnSFxhO7W9pNtOraoJPP/" className="uppercase italic underline">RESUME</a></p>
+            </div>,
           },
         ],
       },
       {
-        title: "Something Else",
+        title: "Work Experience",
         messages: [
           {
             avatar: "/LoggedInUser.jpg",
             name: "Jakob Hoeg",
-            message: "Hey!",
+            message: "Are you currently working somewhere?",
           },
           {
             avatar: "/avatar.gif",
